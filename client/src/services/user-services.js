6e4a34c8c10e02491bc8cad475b4338a5eda75d8
Deepak8218 <128400567+Deepak8218@users.noreply.github.com>
2024-12-02 22:51:01 +0530

    Update user-services.js

--- client/src/services/user-services.js
+++ client/src/services/user-services.js
@@ -1,12 +1,14 @@
 const userService = {
     get: function(id) {
-        return fetch('http://localhost:4000/user/' + id, {
+        // return fetch('http://localhost:4000/user/' + id, {
+         return fetch('https://eazy-bytes-final-project.vercel.app/user/' + id, {
            method: 'GET',
            credentials: 'include'
         }).then(res => res.json());
     },
     register: function(data) {
-        return fetch(`http://localhost:4000/user/register`, {
+        // return fetch(`http://localhost:4000/user/register`, {
+         return fetch(`https://eazy-bytes-final-project.vercel.app/user/register`, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
@@ -16,7 +18,8 @@ const userService = {
         }).then(res => res.json()).then(user => sessionStorage.setItem('user', JSON.stringify(user)));
     },
     login: function(data) {
-        return fetch(`http://localhost:4000/user/login`, {
+        // return fetch(`http://localhost:4000/user/login`, {
+         return fetch(`https://eazy-bytes-final-project.vercel.app/user/login`, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
@@ -28,7 +31,8 @@ const userService = {
         }).then(user => sessionStorage.setItem('user', JSON.stringify(user)));
     },
     logout: function() {
-        return fetch(`http://localhost:4000/user/logout`, {
+        // return fetch(`http://localhost:4000/user/logout`, {
+         return fetch(`https://eazy-bytes-final-project.vercel.app/user/logout`, {
             method: 'POST',
             credentials: 'include'
         }).then(res => res.text()).then(() => sessionStorage.clear());
