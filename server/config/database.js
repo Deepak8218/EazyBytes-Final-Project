741e127d2319b96d8ee784b167357e7782060c44
Deepak8218 <deepakprince917@gmail.com>
2024-12-08 12:38:31 +0530

    Descriptive message for specific files

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    return mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    return mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 };
